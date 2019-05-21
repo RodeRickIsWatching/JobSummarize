790d83eabf24767d6d27c0e776976194aa48fb6f
Roderick <roderick_lu@163.com>
2019-05-22 00:17:48 +0800

    1

--- index.js
+++ index.js
@@ -2,30 +2,36 @@ const Koa = require('koa')
 const fs = require('fs')
 const path = require('path')
 const Router = require('koa-router')
-
+const BodyParse = require('koa-bodyparser')
 
 const app = new Koa();
 const router = new Router()
 
-
-const static_path = './page'
-// app.use(router.routes()).use(router.allowedMethods());
-
-
-app.use(async (ctx, next) => {
-    let full_path = path.join(__dirname, static_path)
-    let content;
-    console.log(ctx)
-    let baseUrl = ctx.originalUrl.split('?')[0]
-    try {
-        content = fs.readFileSync(`${full_path}/${baseUrl}`)
-    }catch(e){
-        content = '<h1>404! 404!</h1>'
-    }
-    ctx.res.writeHead(200)
-    ctx.res.write(content)
-    ctx.res.end()
+router.get('/', (ctx) => {
+    ctx.redirect('/index')
 })
 
+router.get('/index', ctx => {
+    ctx.body = fs.readFileSync(path.join(__dirname, './page/index.html'), 'utf-8')
+})
+router.get('/test', ctx => {
+    ctx.body = fs.readFileSync(path.join(__dirname, './page/test.html'), 'utf-8')
+})
+router.get('/index/getapi', ctx => {
+    let query = ctx.request.query
+    console.log(query)
+    ctx.response.status = 200;
+    ctx.response.body = query
+})
+router.post('/index/postapi',ctx => {
+    let query = ctx.request.body
+    console.log(query)
+    ctx.response.status = 200;
+    ctx.response.body = query
+})
+
+
+app.use(BodyParse())
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(12306)
